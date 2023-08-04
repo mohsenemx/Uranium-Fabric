078708d066b1d6895d4e3c6898b9f1fee7ba2d35
Mohsen Taghavi <78661162+mohsenemx@users.noreply.github.com>
2023-08-04 23:25:20 +0330

    Initial commit

---
+++ src/main/java/net/mohsenemx/uranium/UraniumMod.java
@@ -0,0 +1,16 @@
+package net.mohsenemx.uranium;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UraniumMod implements ModInitializer {
+	public static final String MOD_ID = "uranium";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/mohsenemx/uranium/UraniumModClient.java
@@ -0,0 +1,11 @@
+package net.mohsenemx.uranium;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UraniumModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
